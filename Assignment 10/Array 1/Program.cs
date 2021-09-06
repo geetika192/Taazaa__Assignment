40d16456a0a3f8c9b8c214c9b739d207149fc0e1
Geetika_Shukla <geetikashukla192@gmail.com>
2021-09-06 11:43:02 +0530

    Database

---
+++ Assignment 10/Array 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Array1
+{
+    class Program
+    {
+        static int sum(int []arr, int n)
+    {
+         
+        int sum = 0; 
+         
+        
+        
+        for (int i = 0; i < n; i++)
+            sum += arr[i];
+         
+        return sum;
+    }
+    public static void Main()
+    { 
+        int []arr = {12,3,4,15};
+        int n = arr.Length;
+        Console.Write("Sum of given array is "+ sum(arr, n));
+    }
+ 
+    }
+}
---
+++ Assignment 10/Array 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Array3
+{
+    class Program
+    {
+         public static void Main(string[] args)  
+    {  
+        int[] arr = { 10, 20, 30, 40, 50 };  
+   
+         
+        foreach (int i in arr)  
+        {  
+            Console.WriteLine(i);  
+        }  
+    }  
+    }
+}
